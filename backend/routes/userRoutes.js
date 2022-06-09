1756b485ecb50ae988ad074e81b4fab55d9ac6ff
skouroliakos <thanosmw2@gmail.com>
2022-06-09 18:17:55 +0300

    update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -110,7 +110,7 @@ userRouter.put(
   '/profile',
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const user = await User.findById(mongoose.Types.ObjectId(id).sreq.user._id);
+    const user = await User.findById(req.user._id);
     if (user) {
       user.name = req.body.name || user.name;
       user.email = req.body.email || user.email;
