5fdab599e663c880005e2e86ca66831942ffada4
skouroliakos <thanosmw2@gmail.com>
2022-06-09 16:28:15 +0300

    update

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -19,7 +19,6 @@ uploadRouter.post(
       api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
     });
-
     const streamUpload = (req) => {
       return new Promise((resolve, reject) => {
         const stream = cloudinary.uploader.upload_stream((error, result) => {
